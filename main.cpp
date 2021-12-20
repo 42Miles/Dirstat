1c82bb1609d6451a1afd57ae63ace0b22a8d7e90
Miles <aistra200050@gmail.com>
2021-12-20 17:38:49 +0100

    Add getDirFiles in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,30 @@
 #include <filesystem>
 #include <thread>
 #include <vector>
+#include <algorithm>
 
 namespace fs = std::filesystem;
 
+std::vector<std::string> getDirFiles(const fs::path & dir)
+{
+    std::vector<std::string> files;
+    for(auto & file: fs::recursive_directory_iterator(dir))
+    {
+        if(fs::is_regular_file(file))
+        {
+            files.push_back(file.path().string());
+        }
+    }
+    return files;
+}
+
 int main()
 {
+    std::vector<std::string> f = getDirFiles(fs::current_path());
+    for(const std::string &s: f)
+    {
+    std::cout << s << std::endl;
+    }
 
     return 0;
 }
