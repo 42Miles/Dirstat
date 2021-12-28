4732a1d51443723a0c20f3b7dec0662485acee53
Miles <aistra200050@gmail.com>
2021-12-28 22:48:13 +0100

    Add more complicated tests

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -1,11 +1,11 @@
 #include "../src/dirstat.hpp"
 #include "gtest/gtest.h"
 
-TEST(ApplicationTest, EmptyFiles)
+TEST(Test, EmptyFiles)
 {
-    fs::create_directories("./SimpleTest/test");
+    fs::create_directories("./SimpleTest/EmptyFiles");
     std::string user_input = fs::current_path().c_str();
-    user_input += "/SimpleTest/test/";
+    user_input += "/SimpleTest/EmptyFiles/";
 
     size_t count = 10;
 
@@ -16,9 +16,103 @@ TEST(ApplicationTest, EmptyFiles)
     Application simpleTest(user_input);
     simpleTest.getDir();
     simpleTest.checkDirectory();
-    simpleTest.getDirFiles();
+    simpleTest.countDirFiles();
+    simpleTest.countAllLines();
+    simpleTest.countAllEmptyLines();
 
-    EXPECT_EQ(simpleTest.files.size(), count);
+    EXPECT_EQ(simpleTest.getFiles().size(), count);
+    EXPECT_EQ(simpleTest.getAll_lines(), 0);
+    EXPECT_EQ(simpleTest.getEmpty_lines(), 0);
+
+    fs::remove_all("SimpleTests");
+}
+
+TEST(SimpleTest, EmptyDirectoy)
+{
+    fs::create_directories("./SimpleTest/EmptyDirectory");
+    std::string user_input = fs::current_path().c_str();
+    user_input += "/SimpleTest/EmptyDirectory/";
+
+    Application simpleTest(user_input);
+    simpleTest.getDir();
+    simpleTest.checkDirectory();
+    simpleTest.countDirFiles();
+    simpleTest.countAllLines();
+    simpleTest.countAllEmptyLines();
+
+    EXPECT_EQ(simpleTest.getFiles().size(), 0);
+    EXPECT_EQ(simpleTest.getAll_lines(), 0);
+    EXPECT_EQ(simpleTest.getEmpty_lines(), 0);
+
+    fs::remove_all("SimpleTest");
+}
+
+TEST(SimpleTest, FewLines)
+{
+    fs::create_directories("./SimpleTest/FewLines");
+    std::string user_input = fs::current_path().c_str();
+    user_input += "/SimpleTest/FewLines/";
+
+    size_t count = 10;
+    int total = 0;
+
+    for(size_t i = 0; i < count; ++i)
+    {
+        std::ofstream test_file { user_input + "test_file" + std::to_string(i) + ".txt" };
+        test_file << "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
+        test_file << "Quisque imperdiet posuere ligula, vel tincidunt neque venenatis vitae.\n";
+        test_file << "Duis laoreet risus in nisl luctus efficitur. \n";
+        test_file << "\n";
+        test_file << "Sed luctus quam a dui dignissim mattis.\n";
+        total += 5;
+    }
+
+    Application simpleTest(user_input);
+    simpleTest.getDir();
+    simpleTest.checkDirectory();
+    simpleTest.countDirFiles();
+    simpleTest.countAllLines();
+    simpleTest.countAllEmptyLines();;
+
+    EXPECT_EQ(simpleTest.getFiles().size(), count);
+    EXPECT_EQ(simpleTest.getAll_lines(), total);
+    EXPECT_EQ(simpleTest.getEmpty_lines(), total / 5);
+
+    fs::remove_all("SimpleTest");
+}
+
+TEST(SimpleTest, LargeNumberOfLines)
+{
+    fs::create_directories("./SimpleTest/LargeNumberOfLines");
+    std::string user_input = fs::current_path().c_str();
+    user_input += "/SimpleTest/LargeNumberOfLines/";
+
+    size_t count = 10;
+    int total = 0;
+
+    for(size_t i = 0; i < count; ++i){
+        std::ofstream test_file { user_input + "test_file" + std::to_string(i) + ".txt" };
+        for(size_t j = 0; j < count * 2; ++j)
+        {
+        test_file << "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
+        test_file << "Quisque imperdiet posuere ligula, vel tincidunt neque venenatis vitae.\n";
+        test_file << "Duis laoreet risus in nisl luctus efficitur. \n";
+        test_file << "\n";
+        test_file << "Sed luctus quam a dui dignissim mattis.\n";
+        total += 5;
+        }
+    }
+
+    Application simpleTest(user_input);
+    simpleTest.getDir();
+    simpleTest.checkDirectory();
+    simpleTest.countDirFiles();
+    simpleTest.countAllLines();
+    simpleTest.countAllEmptyLines();
+
+    EXPECT_EQ(simpleTest.getFiles().size(), count);
+    EXPECT_EQ(simpleTest.getAll_lines(), total);
+    EXPECT_EQ(simpleTest.getEmpty_lines(), total /5);
 
     fs::remove_all("SimpleTest");
 }
