bee7540662fd1f725a63cf5b231792fd1c9fc959
Miles <aistra200050@gmail.com>
2021-12-28 22:47:56 +0100

    Add getters and setters for private variables And change names for some methods

--- src/dirstat.hpp
+++ src/dirstat.hpp
@@ -16,15 +16,11 @@ public:
 
     void getDir();
 
-    unsigned int countLinesInFile(const std::string & file_path);
+    void countDirFiles();
 
-    unsigned int countEmptyLinesInFile(const std::string & file_path);
+    void countAllLines();
 
-    void getDirFiles();
-
-    void getAllLines();
-
-    void getAllEmptyLines();
+    void countAllEmptyLines();
 
     inline bool isExistsing ();
 
@@ -38,16 +34,26 @@ public:
 
     ~Application();
 
+    const std::vector<std::string> &getFiles() const;
+
+    const std::string &getDirectory() const;
+
+    unsigned int getAll_lines() const;
+
+    unsigned int getEmpty_lines() const;
+private:
+
     std::vector<std::string> files;
 
     std::string directory;
-
-private:
 
     unsigned int all_lines;
 
     unsigned int empty_lines;
 
+    unsigned int countLinesInFile(const std::string & file_path);
+
+    unsigned int countEmptyLinesInFile(const std::string & file_path);
 };
 
 #endif // MAIN_H
